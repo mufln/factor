55e654434336c5b995c7bc4d960c441aba8038b1
peachonka <okneverak@gmail.com>
2024-09-25 16:45:03 +0300

    я пыталась разобраться с маршрутизацией

--- react-spa-2/src/App.js
+++ react-spa-2/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login'
+import Register from './pages/register';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Routes>
+        {/* подстановочный путь */}
+        <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
